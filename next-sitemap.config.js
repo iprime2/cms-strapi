b6375355ba1d996cb56f297c7c0f0f4a1817e6f2
Sushil Kumar Gupta <kalwarsushil2@gmail.com>
2024-09-13 01:14:16 +0530

    SEO fix 3

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 // next-sitemap.config.js
 module.exports = {
-    siteUrl: 'https://https://cms-strapi-mocha.vercel.app', // Replace with your domain
+    siteUrl: 'https://cms-strapi-mocha.vercel.app', // Replace with your domain
     generateRobotsTxt: true, // Generates robots.txt file
     changefreq: 'daily', // How often the content changes
     priority: 0.7, // Page priority (0.0 - 1.0)
